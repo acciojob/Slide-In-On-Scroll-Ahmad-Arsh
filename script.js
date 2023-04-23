29ecfa647b739ee5745ccbd44df33b0f4d4b6424
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-23 17:27:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // Your JS code here.
+ function debounce(func, wait = 20, immediate = true) {
+      let timeout;
+      return function() {
+        const context = this, args = arguments;
+        const later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
+
+    function checkSlide(e) {
+      const images = document.querySelectorAll('.slide-in');
+      images.forEach(image => {
+        // halfway through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
+        // bottom of the image
+        const imageBottom = image.offsetTop + image.height;
+        const isHalfShown = slideInAt > image.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+          image.classList.add('active');
+        } else {
+          image.classList.remove('active');
+        }
+      });
+    }
+
+    window.addEventListener('scroll', debounce(checkSlide));
